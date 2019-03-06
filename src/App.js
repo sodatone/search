30ad587ee4684dcf08f5a8fd9aeb5a7853eb59a5
Arjun Bali <arjun.bali96@gmail.com>
2019-03-06 17:26:25 -0500

    Add API_URL and API_KEY

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// API Docs: https://www.last.fm/api/show/artist.search
+const API_URL = "http://ws.audioscrobbler.com/2.0/"
+const API_KEY = "430711e29ad09c493dad2831eb0bbd08"
+
 class App extends Component {
   render() {
     return (
